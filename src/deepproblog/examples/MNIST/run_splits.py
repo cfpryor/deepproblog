c9e7b956439f89b5368ec30c56a9ddb03aef6bcb
Connor Pryor <cfpryor@ucsc.edu>
2022-05-10 07:31:16 -0700

    Set up infustructure to run test splits.

--- src/deepproblog/examples/MNIST/run_splits.py
+++ src/deepproblog/examples/MNIST/run_splits.py
@@ -11,11 +11,15 @@ DATA_DIR = os.path.join(THIS_DIR, '..', '..', '..', '..', '..', 'psl_data',
 RESULTS_DIR = os.path.join(THIS_DIR, 'results')
 LOG_DIR = os.path.join(THIS_DIR, 'log')
 
-EVAL_DIR = 'eval'
-LEARN_DIR = 'learn'
+EVAL = 'eval'
+LEARN = 'learn'
+TEST = 'test'
 IMAGESUM_FILENAME = 'imagesum_truth.txt'
-CONFIG_FILENAME = 'config.json'
-LOG_FILENAME = 'out.log'
+CONFIG_FILENAMES = {EVAL: 'config.json', TEST: 'test_config.json'}
+LOG_FILENAMES = {EVAL: 'out.log', TEST: 'test_out.log'}
+
+EPOCHS = {1: {20: 30, 37: 30, 75: 40, 150: 40, 300: 50, 3000: 6, 25000: 2},
+          2: {10: 30, 18: 30, 37: 30, 75: 40, 150: 40, 1500: 8, 12500: 3}}
 
 
 def write_json(data, path):
@@ -65,47 +69,62 @@ def main():
                                                        overlap_dir)
                     os.makedirs(overlap_result_path, exist_ok=True)
 
-                    imagesum_learn_path = os.path.join(overlap_path, LEARN_DIR,
+                    imagesum_learn_path = os.path.join(overlap_path, LEARN,
                                                        IMAGESUM_FILENAME)
-                    imagesum_eval_path = os.path.join(overlap_path, EVAL_DIR,
+                    imagesum_eval_path = os.path.join(overlap_path, EVAL,
+                                                      IMAGESUM_FILENAME)
+                    imagesum_test_path = os.path.join(overlap_path, TEST,
                                                       IMAGESUM_FILENAME)
 
-                    config_path = os.path.join(overlap_result_path,
-                                               CONFIG_FILENAME)
-                    log_path = os.path.join(overlap_result_path, LOG_FILENAME)
-                    print("Starting Run: %s" % (overlap_path,))
-                    if os.path.isfile(config_path):
-                        print("Found existing config file, skipping run: %s" % (
-                            overlap_path,))
-                        continue
+                    for test_val_key in CONFIG_FILENAMES:
+                        config_path = os.path.join(overlap_result_path,
+                                                   CONFIG_FILENAMES[test_val_key])
+                        log_path = os.path.join(overlap_result_path,
+                                                LOG_FILENAMES[test_val_key])
+                        print("Starting Run: %s" % (overlap_path,))
+                        if os.path.isfile(config_path):
+                            print("Found existing config file for %s, skipping run: %s" % (CONFIG_FILENAMES[test_val_key], overlap_path,))
+                            continue
 
-                    train_indices = load_data(imagesum_learn_path)
-                    test_indices = load_data(imagesum_eval_path)
+                        train_indices = load_data(imagesum_learn_path)
+                        val_indices = load_data(imagesum_eval_path)
+                        test_indices = load_data(imagesum_test_path)
 
-                    time_start = time.time()
-                    if train_size >= 1000:
-                        num_epochs = 5
-                    else:
-                        num_epochs = 15
-                    config = addition.main(num_digits=num_digits,
-                                           seed=fold,
-                                           num_epochs=num_epochs,
-                                           batch_size=2,
-                                           learning_rate=1e-3,
-                                           log_iterations=train_size // 5,
-                                           train_indices=train_indices,
-                                           test_indices=test_indices,
-                                           train_set_name="train",
-                                           test_set_name="train")
-                    time_end = time.time()
-                    config["program_time_start"] = time_start
-                    config["program_time_end"] = time_end
-                    config["program_total_time"] = (time_end - time_start)
+                        time_start = time.time()
 
-                    saved_log_path = os.path.join(LOG_DIR, config["name"] + ".log")
+                        if test_val_key == EVAL:
+                            config = addition.main(num_digits=num_digits,
+                                                   seed=fold,
+                                                   num_epochs=EPOCHS[num_digits][train_size],
+                                                   batch_size=2,
+                                                   learning_rate=1e-3,
+                                                   log_iterations=train_size // 5,
+                                                   train_indices=train_indices,
+                                                   test_indices=val_indices,
+                                                   train_set_name="train",
+                                                   test_set_name="train")
+                        if test_val_key == TEST:
+                            config = addition.main(num_digits=num_digits,
+                                                   seed=fold,
+                                                   num_epochs=EPOCHS[num_digits][train_size],
+                                                   batch_size=2,
+                                                   learning_rate=1e-3,
+                                                   log_iterations=train_size // 5,
+                                                   train_indices=train_indices,
+                                                   test_indices=test_indices,
+                                                   train_set_name="train",
+                                                   test_set_name="test")
 
-                    os.rename(saved_log_path, log_path)
-                    write_json(config, config_path)
+                        time_end = time.time()
+                        config["program_time_start"] = time_start
+                        config["program_time_end"] = time_end
+                        config["program_total_time"] = (time_end - time_start)
+
+                        saved_log_path = os.path.join(LOG_DIR,
+                                                      config["name"] + ".log")
+
+                        os.rename(saved_log_path, log_path)
+                        write_json(config, config_path)
 
 
 if __name__ == '__main__':
