e7fce73fec44d0b9cb6e8fa1ad7e640eed80f6b4
Connor Pryor <cfpryor@ucsc.edu>
2023-05-10 21:24:06 -0700

    Updated run script to be compatable with ijcai 2023 NeuPSL submission.

--- src/deepproblog/examples/MNIST/run_splits.py
+++ src/deepproblog/examples/MNIST/run_splits.py
@@ -7,17 +7,9 @@ import time
 import addition
 
 THIS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
-DATA_DIR = os.path.join(THIS_DIR, '..', '..', '..', '..', '..', 'psl_data',
-                        'mnist-addition')
+DATA_DIR = os.path.join(THIS_DIR, '..', '..', '..', '..', '..', 'psl_data', 'mnist-addition')
 RESULTS_DIR = os.path.join(THIS_DIR, 'results')
 LOG_DIR = os.path.join(THIS_DIR, 'log')
-
-EVAL = 'eval'
-LEARN = 'learn'
-TEST = 'test'
-IMAGESUM_FILENAME = 'imagesum_truth.txt'
-CONFIG_FILENAMES = {EVAL: 'config.json', TEST: 'test_config.json'}
-LOG_FILENAMES = {EVAL: 'out.log', TEST: 'test_out.log'}
 
 EPOCHS = {1: {20: 30, 30: 30, 37: 30, 40: 30, 75: 40, 150: 40, 300: 20, 3000: 6, 25000: 10},
           2: {10: 30, 15: 30, 18: 30, 20: 30, 37: 30, 75: 40, 150: 40, 1500: 8, 12500: 10}}
@@ -50,20 +42,19 @@ def main():
         os.makedirs(experiment_result_path, exist_ok=True)
         num_digits = int(experiment_dir[-1:])
 
-        for fold_dir in sorted(os.listdir(experiment_path)):
-            fold_path = os.path.join(experiment_path, fold_dir)
-            if os.path.isfile(fold_path):
+        for split_dir in sorted(os.listdir(experiment_path)):
+            split_path = os.path.join(experiment_path, split_dir)
+            if os.path.isfile(split_path):
                 continue
-            fold_result_path = os.path.join(experiment_result_path, fold_dir)
-            os.makedirs(fold_result_path, exist_ok=True)
-            fold = int(fold_path[-2:])
-            if fold < 0:
+            split_result_path = os.path.join(experiment_result_path, split_dir)
+            os.makedirs(split_result_path, exist_ok=True)
+            split = int(split_path[-2:])
+            if split < 0:
                 continue
 
-            for train_size_dir in sorted(os.listdir(fold_path)):
-                train_size_path = os.path.join(fold_path, train_size_dir)
-                train_size_result_path = os.path.join(fold_result_path,
-                                                      train_size_dir)
+            for train_size_dir in sorted(os.listdir(split_path)):
+                train_size_path = os.path.join(split_path, train_size_dir)
+                train_size_result_path = os.path.join(split_result_path, train_size_dir)
                 os.makedirs(train_size_result_path, exist_ok=True)
                 train_size = int(train_size_path[-5:])
 
@@ -73,65 +64,41 @@ def main():
                                                        overlap_dir)
                     os.makedirs(overlap_result_path, exist_ok=True)
 
-                    imagesum_learn_path = os.path.join(overlap_path, LEARN,
-                                                       IMAGESUM_FILENAME)
-                    imagesum_eval_path = os.path.join(overlap_path, EVAL,
-                                                      IMAGESUM_FILENAME)
-                    imagesum_test_path = os.path.join(overlap_path, TEST,
-                                                      IMAGESUM_FILENAME)
+                    imagesum_learn_path = os.path.join(overlap_path, 'image-sum-truth-train.txt')
+                    imagesum_eval_path = os.path.join(overlap_path, 'image-sum-truth-test.txt')
 
-                    for test_val_key in CONFIG_FILENAMES:
-                        config_path = os.path.join(overlap_result_path,
-                                                   CONFIG_FILENAMES[test_val_key])
-                        log_path = os.path.join(overlap_result_path,
-                                                LOG_FILENAMES[test_val_key])
-                        print("Starting Run: %s" % (overlap_path,))
-                        if os.path.isfile(config_path):
-                            print("Found existing config file for %s, skipping run: %s" % (CONFIG_FILENAMES[test_val_key], overlap_path,))
-                            continue
+                    config_path = os.path.join(overlap_result_path, "config.json")
+                    log_path = os.path.join(overlap_result_path, "out.txt")
+                    print("Starting Run: %s" % (overlap_path,))
+                    if os.path.isfile(config_path):
+                        print("Found existing config file, skipping run: %s" % (overlap_path,))
+                        continue
 
-                        train_indices = load_data(imagesum_learn_path)
-                        val_indices = load_data(imagesum_eval_path)
-                        test_indices = load_data(imagesum_test_path)
+                    train_indices = load_data(imagesum_learn_path)
+                    val_indices = load_data(imagesum_eval_path)
 
-                        time_start = time.time()
+                    time_start = time.time()
+                    config = addition.main(method=METHOD,
+                                           num_digits=num_digits,
+                                           seed=split,
+                                           num_epochs=EPOCHS[num_digits][train_size],
+                                           batch_size=2,
+                                           learning_rate=1e-3,
+                                           log_iterations=train_size // 5,
+                                           train_indices=train_indices,
+                                           test_indices=val_indices,
+                                           train_set_name="train",
+                                           test_set_name="train")
+                    time_end = time.time()
 
-                        if test_val_key == EVAL:
-                            config = addition.main(method=METHOD,
-                                                   num_digits=num_digits,
-                                                   seed=fold,
-                                                   num_epochs=EPOCHS[num_digits][train_size],
-                                                   batch_size=2,
-                                                   learning_rate=1e-3,
-                                                   log_iterations=train_size // 5,
-                                                   train_indices=train_indices,
-                                                   test_indices=val_indices,
-                                                   train_set_name="train",
-                                                   test_set_name="train")
-                        if test_val_key == TEST:
-                            # config = addition.main(method=METHOD,
-                            #                        num_digits=num_digits,
-                            #                        seed=fold,
-                            #                        num_epochs=EPOCHS[num_digits][train_size],
-                            #                        batch_size=2,
-                            #                        learning_rate=1e-3,
-                            #                        log_iterations=train_size // 5,
-                            #                        train_indices=train_indices,
-                            #                        test_indices=test_indices,
-                            #                        train_set_name="train",
-                            #                        test_set_name="test")
-                            continue
+                    config["program_time_start"] = time_start
+                    config["program_time_end"] = time_end
+                    config["program_total_time"] = (time_end - time_start)
 
-                        time_end = time.time()
-                        config["program_time_start"] = time_start
-                        config["program_time_end"] = time_end
-                        config["program_total_time"] = (time_end - time_start)
+                    saved_log_path = os.path.join(LOG_DIR, config["name"] + ".log")
 
-                        saved_log_path = os.path.join(LOG_DIR,
-                                                      config["name"] + ".log")
-
-                        shutil.move(saved_log_path, log_path)
-                        write_json(config, config_path)
+                    shutil.move(saved_log_path, log_path)
+                    write_json(config, config_path)
 
 
 if __name__ == '__main__':
